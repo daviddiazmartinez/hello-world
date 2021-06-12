ec8a650d7b61fbfce22174443e5cb5129ab4a0a3
David Díaz <david.diaz.martinz@gmail.com>
2021-06-12 11:10:01 +0200

    change Hello World to GoodBye

--- src/com/ironhack/main/Main.java
+++ src/com/ironhack/main/Main.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World 2");
+        System.out.println("Good bye World 2");
         int day=5;
 
         switch (day){
